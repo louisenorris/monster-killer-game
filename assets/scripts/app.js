5c1492a61ead5ca2cea54244a0e345d63d287403
Louise Norris <louisenorris73@yahoo.co.uk>
2020-04-24 09:27:24 +0100

    added in bonus life functionality to endRound function and built reset function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,18 +6,39 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
+function reset() {
+    currentMonsterHealth = chosenMaxLife;
+    currentPlayerHealth = chosenMaxLife;
+    resetGame(chosenMaxLife);
+}
+
 function endRound() {
+    const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage
+
+    if (currentPlayerHealth <= 0 && hasBonusLife) {
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        alert('You would be dead but the bonus life saved you!');
+        setPlayerHealth(initialPlayerHealth);
+    }
+
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert('You Won!');
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
         alert('You Lost!');
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
         alert('You have a draw!');
+    }
+
+    if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
+        reset();
     }
 }
 
